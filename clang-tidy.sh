9a59286fbe3298d1fe196792fd1a3101ab0032fe
Milosz Wasilewski <milosz.wasilewski@foundries.io>
2022-11-14 11:10:22 +0000

    Remove DEFAULT_TAG, AKLITE_TAGS and DEFAULT_FACTORY
    Default tag can be read from the filesystem at runtime. When it's not
    possible to retreive from the files, lmp-device-register will prompt
    user for input.
    
    Factory is no longer required as a build time parameter. Instead
    lmp-device-register will attempt to read the value from user input,
    environment variable or /etc/os-release file (in this order).
    
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $(readlink -f $0))
 
-clang-tidy-6.0 ./src/main.cpp -- $(pkg-config --cflags glib-2.0) -DDEVICE_FACTORY='"foo"' -DDEVICE_API='"url"' -DDOCKER_COMPOSE_APP="1" -DAKLITE_TAGS="1" -DHARDWARE_ID='"foo"' -DGIT_COMMIT='"deadbeef"'
+clang-tidy-6.0 ./src/main.cpp -- $(pkg-config --cflags glib-2.0) -DDEVICE_API='"url"' -DDOCKER_COMPOSE_APP="1" -DHARDWARE_ID='"foo"' -DGIT_COMMIT='"deadbeef"'
