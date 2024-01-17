foundriesio/lmp-device-register

commit 263d11e77d0489e0daa8ed515eb5b4f2a6ad835d

main: refactor lmp-device-register
Uses libp11 and the openssl API instead of the helper programs
to access pkcs#11 and openssl.

Needs one fix to libp11 (for token initialization) and one missing
feature (EC key generation, proposed upstream) plus a fix required
to support TPM.

PKCS11 support can be compiled out with -DDISABLE_PKCS11=ON.

Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
Co-developed-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

src/auth.cpp
@@ -1,0 +1,143 @@
+/*
+ * Copyright (c) 2023 Foundries.io
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <device_register.h>
+#include <curl.hpp>
+
+namespace b64 = boost::beast::detail::base64;
+using boost::property_tree::ptree;
+
+static string get_oauth_token(const string &factory, const string &device_uuid)
+{
+	const char *env = getenv(ENV_OAUTH_BASE);
+	char WHEELS[] = { '|', '/', '-', '\\' };
+	std::map<string, string> headers;
+	string data;
+	string url;
+	ptree json;
+
+	url = env == nullptr ? "https://app.foundries.io/oauth" : env;
+
+	data = "client_id=" + device_uuid;
+	data += "&scope=" + factory + ":devices:create";
+
+	gint64 code = Curl(url + "/authorization/device/").Post(headers, data, json);
+	if (code != 200) {
+		cerr << "Unable to create device authorization request: HTTP_" << code << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	cout << endl;
+	cout << "----------------------------------------------------------------------------" << endl;
+	cout << "Visit the link below in your browser to authorize this new device. This link" << endl;
+	cout << "will expire in " << (json.get<int>("expires_in") / 60) << " minutes." << endl;
+	cout << "  Device Name: " << device_uuid << endl;
+	cout << "  User code: " << json.get<string>("user_code") << endl;
+	cout << "  Browser URL: " << json.get<string>("verification_uri") << endl;
+	cout << endl;
+
+	data = "grant_type=urn:ietf:params:oauth:grant-type:device_code";
+	data += "&device_code=" + json.get<string>("device_code");
+	data += "&client_id=" + device_uuid;
+	data += "&scope=" + factory + ":devices:create";
+
+	string msg;
+	int i = 0;
+	int interval = json.get<int>("interval");
+
+	while (true) {
+		gint64 code = Curl(url + "/token/").Post(headers, data, json);
+
+		if (code == 200)
+			return json.get<string>("access_token");
+
+		if (code != 400) {
+			cout << "HTTP(" << code << ") error..." << endl;
+			sleep(2);
+			continue;
+		}
+
+		/* Process */
+		if (json.get<string>("error") == "authorization_pending") {
+			cout << "Waiting for authorization ";
+			cout << WHEELS[i++ % sizeof(WHEELS)] << "\r" << std::flush;
+			sleep(interval);
+		} else {
+			cerr << "Error authorizing device: ";
+			cerr << json.get<string>("error_description") << endl;
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+/*
+ * Headers of a request to the device registration endpoint DEVICE_API
+ * Default https://api.foundries.io/ota/devices/
+ */
+void auth_get_http_headers(lmp_options &opt, http_headers &headers)
+{
+	if (!opt.api_token.empty()) {
+		headers[opt.api_token_header] = opt.api_token;
+		return;
+	}
+
+	/*
+	 * If a token is not specified as a command line parameter then try
+	 * to get an oauth token from the Foundries' auth endpoint
+	 * https://app.foundries.io/oauth/authorization/device/
+	 */
+	cout << "Foundries providing auth token " << endl;
+	string token = get_oauth_token(opt.factory, opt.uuid);
+	string token_base64;
+
+	token_base64.resize(b64::encoded_size(token.size()));
+	b64::encode(&token_base64[0], token.data(), token.size());
+
+	headers["Authorization"] = "Bearer " + token_base64;
+}
+
+int auth_register_device(http_headers &headers, ptree &device, ptree &resp)
+{
+	const char *api = std::getenv(ENV_DEVICE_API);
+	stringstream data;
+	gint64 code;
+
+	if (api == nullptr)
+		api = DEVICE_API;
+
+	write_json(data, device);
+	code = Curl(api).Post(headers, data.str(), resp);
+	if (code != 201) {
+		cerr << "Unable to create device: HTTP_" << code << endl;
+		if (resp.data().length())
+			cerr << resp.data() << endl;
+
+		for (auto it: resp)
+			cerr << it.first << ": " << it.second.data() << endl;
+
+		return -1;
+	}
+
+	return 0;
+}
+
+int auth_ping_server(void)
+{
+	/* Get the device API from the environment */
+	const char *api = std::getenv(ENV_DEVICE_API);
+	if (api == nullptr)
+		api = DEVICE_API;
+
+	cout << "Using DEVICE_API: " << api << endl;
+	const auto ping_res{Curl(api).PingEndpoint()};
+
+	if (!std::get<0>(ping_res)) {
+		cerr << std::get<1>(ping_res) << endl;
+		return -1;
+	}
+
+	return 0;
+}

CMakeLists.txt
@@ -46,19 +46,25 @@ message(STATUS "Setting SOTA_CLIENT to ${SOTA_CLIENT}")
 add_definitions('-g')
 add_definitions('-Wall')
 
-set(CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake-modules)
-
 # find all required libraries
 set(Boost_USE_STATIC_LIBS ON)
 find_package(Boost COMPONENTS filesystem iostreams program_options system REQUIRED)
 find_package(CURL REQUIRED)
 find_package(PkgConfig REQUIRED)
+find_package(OpenSSL 3.0.0 REQUIRED)
 pkg_search_module(GLIB REQUIRED glib-2.0)
 
 # Use C++11, but without GNU or other extensions
 set(CMAKE_CXX_STANDARD 11)
 
-add_executable(lmp-device-register src/main.cpp)
-target_include_directories(lmp-device-register PRIVATE ${GLIB_INCLUDE_DIRS})
-target_link_libraries(lmp-device-register ${CURL_LIBRARIES} ${Boost_LIBRARIES} ${GLIB_LDFLAGS})
+if(DISABLE_PKCS11)
+add_executable(lmp-device-register src/main.cpp src/options.cpp src/auth.cpp src/pkcs11_stub.cpp src/openssl.cpp)
+target_link_libraries(lmp-device-register ${CURL_LIBRARIES} ${Boost_LIBRARIES} ${GLIB_LDFLAGS} ${OPENSSL_LIBRARIES})
+else(DISABLE_PKCS11)
+pkg_check_modules(LIBP11 REQUIRED libp11)
+add_executable(lmp-device-register src/main.cpp src/options.cpp src/auth.cpp src/pkcs11.cpp src/openssl.cpp)
+target_link_libraries(lmp-device-register ${CURL_LIBRARIES} ${Boost_LIBRARIES} ${GLIB_LDFLAGS} ${OPENSSL_LIBRARIES} ${LIBP11_LIBRARIES})
+endif(DISABLE_PKCS11)
+
+target_include_directories(lmp-device-register PRIVATE $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/inc> ${GLIB_INCLUDE_DIRS})
 install(TARGETS lmp-device-register RUNTIME DESTINATION bin)

inc/curl.hpp
@@ -1,0 +1,133 @@
+/*
+ * Copyright (c) 2023 Foundries.io
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef CURL_H
+#define CURL_H
+
+#include <boost/algorithm/string.hpp>
+#include <boost/beast/core/detail/base64.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/program_options.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/interprocess/sync/file_lock.hpp>
+
+#include <curl/curl.h>
+
+using boost::property_tree::ptree;
+using std::stringstream;
+using std::string;
+using std::cerr;
+using std::cout;
+using std::endl;
+
+static size_t write_sstream(void *buf, size_t size, size_t nmemb, void *userp)
+{
+	auto *body = static_cast<stringstream *>(userp);
+
+	body->write(static_cast<const char *>(buf), size * nmemb);
+
+	return size * nmemb;
+}
+
+class Curl {
+private:
+	string _url;
+public:
+	Curl(const string &url)
+	{
+		_url = url;
+		curl_global_init(CURL_GLOBAL_DEFAULT);
+		curl = curl_easy_init();
+		if (curl != nullptr) {
+			curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		}
+	}
+	~Curl()
+	{
+		if (curl != nullptr) {
+			curl_easy_cleanup(curl);
+		}
+		curl_global_cleanup();
+	}
+	void ParseResponse(stringstream &body, ptree &resp)
+	{
+		try {
+			read_json(body, resp);
+		} catch (const boost::property_tree::json_parser::json_parser_error &e) {
+			cerr << "Unable to parse response from: " << _url << " Error is:" << endl;
+			cerr << " " <<  e.message() << endl;
+			body.seekg(0);
+			cerr << "Raw response was: " << body.str() << endl;
+		}
+	}
+	std::tuple<bool, string> PingEndpoint()
+	{
+		curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
+		CURLcode res = curl_easy_perform(curl);
+		if (res != CURLE_OK) {
+			return { false,
+				"Unable to reach the device registration endpoint " + _url + "; err: " + curl_easy_strerror(res) };
+		}
+		gint64 code = 0;
+		CURLcode get_info_res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &code);
+		if (get_info_res != CURLE_OK) {
+			return { false,
+				"Error while checking the device registration endpoint; err: unable to get curl info: " + string(curl_easy_strerror(get_info_res)) };
+		}
+		if (code >= 500) {
+			// 401 or 400 is returned under normal circumstances what indicates that the OTA backend is reachable and functional
+			return { false,
+				"The device registration endpoint is not healthy" + _url + "; status code: " + std::to_string(code) };
+		}
+		return { true, "" };
+	}
+	gint64 Post(const http_headers &headers, const string &data, ptree &resp)
+	{
+		stringstream body;
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_sstream);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &body);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+
+		struct curl_slist *chunk = nullptr;
+		for (auto item : headers) {
+			string header = item.first + ": " + item.second;
+			chunk = curl_slist_append(chunk, header.c_str());
+		}
+
+		if (chunk != nullptr) {
+			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+		}
+
+		CURLcode res = curl_easy_perform(curl);
+		if (res != CURLE_OK) {
+			cerr << "Unable to post to " << _url << ": " << curl_easy_strerror(res) << endl;
+			exit(1);
+		}
+
+		if (chunk != nullptr) {
+			curl_slist_free_all(chunk);
+		}
+
+		gint64 code = 0;
+		res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &code);
+		if (res != CURLE_OK) {
+			cerr << "Unable to get curl info: " << curl_easy_strerror(res) << endl;
+			exit(1);
+		}
+		ParseResponse(body, resp);
+		return code;
+	}
+private:
+	CURL *curl = nullptr;
+};
+
+#endif

inc/device_register.h
@@ -1,0 +1,115 @@
+/*
+ * Copyright (c) 2023 Foundries.io
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef DEVICE_REGISTER_H
+#define DEVICE_REGISTER_H
+
+#include <exception>
+#include <fcntl.h>
+#include <glib.h>
+#include <iostream>
+#include <regex>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <unistd.h>
+
+#include <openssl/pem.h>
+#include <openssl/evp.h>
+#include <openssl/encoder.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+#include <openssl/err.h>
+#include <openssl/buffer.h>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/beast/core/detail/base64.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/program_options.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/interprocess/sync/file_lock.hpp>
+
+#define __weak __attribute__((weak))
+
+/* OS definitions in os-release */
+#define LMP_OS_STR "/etc/os-release"
+#define OS_FACTORY_TAG "LMP_FACTORY_TAG"
+#define OS_FACTORY "LMP_FACTORY"
+
+/* Environment Variables */
+#define ENV_DEVICE_FACTORY "DEVICE_FACTORY"
+#define ENV_PRODUCTION "PRODUCTION"
+#define ENV_OAUTH_BASE "OAUTH_BASE"
+#define ENV_DEVICE_API "DEVICE_API"
+
+/* HSM defitions */
+#define HSM_TOKEN_STR "aktualizr"
+#define HSM_TLS_STR "tls"
+#define HSM_TLS_ID_STR "01"
+#define HSM_CRT_STR "client"
+#define HSM_CRT_ID 3
+#define HSM_CRT_ID_STR "03"
+
+/* Files */
+#define AKLITE_LOCK "/var/lock/aklite.lock"
+#define SOTA_DIR "/var/sota"
+#define SOTA_PEM "/client.pem"
+#define SOTA_SQL "/sql.db"
+
+using boost::property_tree::ptree;
+using std::stringstream;
+using std::string;
+using std::cerr;
+using std::cout;
+using std::endl;
+
+struct lmp_options {
+	string api_token_header;
+	string device_group;
+	string api_token;
+	string factory;
+	string hwid;
+	string uuid;
+	string name;
+	string hsm_module;
+	string hsm_so_pin;
+	string hsm_pin;
+	string sota_dir;
+	string pacman_tags;
+	bool start_daemon;
+	bool use_server;
+	bool production;
+	bool mlock;
+#if defined DOCKER_COMPOSE_APP
+	string apps;
+	string restorable_apps;
+#endif
+};
+
+typedef std::map<std::string, string> http_headers;
+
+int auth_register_device(http_headers &headers, ptree &device, ptree &resp);
+void auth_get_http_headers(lmp_options &opt, http_headers &headers);
+int auth_ping_server(void);
+
+int options_parse(int argc, char **argv, lmp_options &options);
+
+int openssl_create_csr(const lmp_options &options, string &key, string &csr);
+int openssl_gen_csr(const lmp_options &options, EVP_PKEY *pub, EVP_PKEY *priv,
+		    string &csr);
+
+int pkcs11_create_csr(const lmp_options &options, string &key, string &csr);
+int pkcs11_store_cert(lmp_options &opt, X509 *cert);
+int pkcs11_get_uuid(lmp_options &options);
+#endif

src/openssl.cpp
@@ -1,0 +1,187 @@
+ /*
+ * Copyright (c) 2023 Foundries.io
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <device_register.h>
+
+#define leave \
+({ cerr << "Error !"<< endl; \
+   cerr << "  Commit : " << GIT_COMMIT << endl; \
+   cerr << "  File: openssl.cpp, Func: " << __func__ \
+   << " Line: " << __LINE__ << endl; \
+   return -1; })
+
+static int add_ext(STACK_OF(X509_EXTENSION)*sk, int nid, char *value)
+{
+	X509_EXTENSION *ex = X509V3_EXT_conf_nid(NULL, NULL, nid, value);
+
+	X509_EXTENSION_set_critical(ex, 1);
+	if (!ex)
+		return -1;
+
+	sk_X509_EXTENSION_push(sk, ex);
+	return 0;
+}
+
+/* Use external keys (not created with OpenSSL) to generate a CSR */
+int openssl_gen_csr(const lmp_options &opt, EVP_PKEY *pub, EVP_PKEY *priv,
+		    string &csr)
+{
+	STACK_OF(X509_EXTENSION) *ext = NULL;
+	X509_NAME *name = NULL;;
+	X509_REQ *req = NULL;
+	BUF_MEM *bptr = NULL;
+	BIO *bio = NULL;
+
+	bio = BIO_new(BIO_s_mem());
+	req = X509_REQ_new();
+	name = X509_REQ_get_subject_name(req);
+
+	if (X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC,
+				       (const unsigned char *)
+				       opt.uuid.c_str(), -1, -1, 0) != 1)
+		leave;
+
+	if (X509_NAME_add_entry_by_txt(name, "OU", MBSTRING_ASC,
+				       (const unsigned char *)
+				       opt.factory.c_str(), -1, -1, 0) != 1)
+		leave;
+
+	if (opt.production) {
+		if (X509_NAME_add_entry_by_txt(name, LN_businessCategory,
+					       MBSTRING_ASC,
+					       (const unsigned char *)
+					       "production", -1, -1, 0) != 1)
+			leave;
+	}
+
+	ext = X509_REQ_get_extensions(req);
+	if (!ext)
+		leave;
+
+	if (add_ext(ext, NID_key_usage, (char *)"digitalSignature"))
+		leave;
+
+	if (add_ext(ext, NID_ext_key_usage, (char *)"clientAuth"))
+		leave;
+
+	if (X509_REQ_add_extensions(req, ext) != 1)
+		leave;
+
+	if (X509_REQ_set_pubkey(req, pub) != 1)
+		leave;
+
+	X509_REQ_sign(req, priv, EVP_sha256());
+
+	if (PEM_write_bio_X509_REQ(bio, req) != 1)
+		leave;
+
+	BIO_write(bio, "\0", 1);
+	BIO_get_mem_ptr(bio, &bptr);
+	X509_REQ_free(req);
+
+	/* Output */
+	csr = bptr->data;
+
+	bptr->data = NULL;
+	BIO_free_all(bio);
+
+	return 0;
+}
+
+int openssl_create_csr(const lmp_options &options, string &pkey, string &csr)
+{
+	STACK_OF(X509_EXTENSION) *ext = NULL;
+	X509_NAME *name = NULL;
+	X509_REQ *req = NULL;
+	BUF_MEM *bptr = NULL;
+	BIO *bio = NULL;
+	OSSL_ENCODER_CTX *ectx = NULL;
+	unsigned char *data = NULL;
+	EVP_PKEY_CTX *kctx = NULL;
+	EVP_PKEY *key = NULL;
+	size_t len = 0;
+
+	bio = BIO_new(BIO_s_mem());
+	req = X509_REQ_new();
+	name = X509_REQ_get_subject_name(req);
+
+	if (X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC,
+				       (const unsigned char *)
+				       options.uuid.c_str(), -1, -1, 0) != 1)
+		leave;
+
+	if (X509_NAME_add_entry_by_txt(name, "OU", MBSTRING_ASC,
+				       (const unsigned char *)
+				       options.factory.c_str(), -1, -1, 0) != 1)
+		leave;
+
+
+	if (options.production) {
+		if (X509_NAME_add_entry_by_txt(name, LN_businessCategory,
+					       MBSTRING_ASC,
+					       (const unsigned char *)
+					       "production", -1, -1, 0) != 1)
+			leave;
+	}
+
+	ext = X509_REQ_get_extensions(req);
+
+	if (add_ext(ext, NID_key_usage, (char *)"digitalSignature"))
+		leave;
+
+	if (add_ext(ext, NID_ext_key_usage, (char *)"clientAuth"))
+		leave;
+
+	if (X509_REQ_add_extensions(req, ext) != 1)
+		leave;
+
+	kctx = EVP_PKEY_CTX_new_from_name(NULL, "EC", NULL);
+	if (!kctx)
+		leave;
+
+	if (EVP_PKEY_keygen_init(kctx) <= 0)
+		leave;
+
+	if (EVP_PKEY_CTX_set_group_name(kctx, "prime256v1") <= 0)
+		leave;
+
+	if (EVP_PKEY_keygen(kctx, &key) <= 0)
+		leave;
+
+	ectx = OSSL_ENCODER_CTX_new_for_pkey(key, OSSL_KEYMGMT_SELECT_ALL,
+					     "PEM", NULL, NULL);
+	if (!ectx)
+		leave;
+
+	if (OSSL_ENCODER_to_data(ectx, &data, &len) != 1)
+		leave;
+
+	OSSL_ENCODER_CTX_free(ectx);
+
+	if (X509_REQ_set_pubkey(req, key) != 1)
+		leave;
+
+	if (!X509_REQ_sign(req, key, EVP_sha256()))
+		leave;
+
+	if (PEM_write_bio_X509_REQ(bio, req) != 1)
+		leave;
+
+	EVP_PKEY_free(key);
+	BIO_write(bio, "\0", 1);
+	BIO_get_mem_ptr(bio, &bptr);
+	X509_REQ_free(req);
+
+	/* Output */
+	csr = bptr->data;
+	pkey = string(reinterpret_cast<char *>(data));
+
+	memset(data, 0, len);
+	bptr->data = NULL;
+	BIO_free_all(bio);
+
+	return 0;
+}

src/options.cpp
@@ -1,0 +1,276 @@
+/*
+ * Copyright (c) 2023 Foundries.io
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#include "device_register.h"
+
+using boost::property_tree::ptree;
+
+namespace po = boost::program_options;
+
+#define OPT_DEF_STR(str, option, def, help) \
+(str, po::value<string>(&option)->default_value(def), help)
+
+#define OPT_DEF_BOOL(str, option, def, help) \
+(str, po::value<bool>(&option)->default_value(def), help)
+
+#define OPT_STR(str, option, help) \
+(str, po::value<string>(&option), help)
+
+#define MLOCK_HELP \
+"Avoid paging out the process memory during execution."
+
+#define RESTORABLE_APP_HELP \
+"Configure package-manager for this comma separate list of Restorable Apps. "  \
+"If it is not specified, but a system image is preloaded with Restorable "     \
+"Apps then the Restorable App list is set to an empty list which means "       \
+"turning restorable App usage ON and the resultant list will be equal to the " \
+"apps list. Restorable App list = UNION(compose-apps, restorable-apps)"
+
+#define UUID_HELP \
+"A per-device UUID. If not provided, one will be generated. This is "          \
+"associated with the device, e.g. as the CommonName field in certificates "    \
+"related to it."
+
+#define OSTREE_SRV_HELP \
+"Use OSTree Proxy server instead of the Device Gateway to pull the ostree repo."
+
+#define NAME_HELP \
+"The name of the device as it should appear in the dashboard. When not "       \
+"specified, the device's UUID will be used instead."
+
+#define API_TOKEN_HELP \
+"API token for authentication. If not provided, oauth2 will be used instead."
+
+#define APPS_HELP \
+"Configure package-manager for this comma separate list of apps."
+
+#define TAGS_HELP \
+"Configure " SOTA_CLIENT " to only apply updates from Targets with these tags."
+
+#define DAEMON_HELP \
+"Start the " SOTA_CLIENT " systemd service after registration."
+
+#define SOTA_DIR_HELP \
+"The directory to install to keys and configuration to."
+
+#define API_TOKEN_HDR_HELP \
+"The HTTP header to use for authentication."
+
+#define HWID_HELP \
+"The hardware identifier for the device type."
+
+#define DEVICE_GROUP_HELP \
+"Assign this device to a device group."
+
+#define PRODUCTION_HELP \
+"Mark the device as a production device."
+
+#define FACTORY_HELP \
+"The factory name to subscribe to."
+
+#define HSM_SO_PIN_HELP \
+"The PKCS#11 security officer pin - HSM only."
+
+#define HSM_HELP \
+"The PKCS#11 implementation (.so library) - HSM only."
+
+#define HSM_PIN_HELP \
+"The PKCS#11 pin - HSM only."
+
+static void get_factory_tags_info(const string os_release, string &factory,
+				  string &tag)
+{
+	const char *env = std::getenv(ENV_DEVICE_FACTORY);
+	ptree os_info;
+
+	if (env != nullptr) {
+		cout << "Factory read from environment" << endl;
+		factory = env;
+	}
+
+	if (!boost::filesystem::exists(os_release))
+		return;
+
+	try {
+		read_ini(os_release, os_info);
+	} catch (boost::property_tree::ini_parser_error const &) {
+		cout << "Can't parse file " << os_release << endl;
+	}
+
+	try {
+		tag = os_info.get<std::string>(OS_FACTORY_TAG);
+		boost::algorithm::erase_all(tag, "\"");
+		cout << "Tag read from " << os_release << endl;
+	} catch (boost::property_tree::ptree_bad_path const &) {
+		cout << "Can't read tag from " << os_release << endl;
+	}
+
+	if (!factory.empty())
+		return;
+
+	try {
+		factory = os_info.get<std::string>(OS_FACTORY);
+		boost::algorithm::erase_all(factory, "\"");
+		cout << "Factory read from " << os_release << endl;
+	} catch (boost::property_tree::ptree_bad_path const &) {
+		cout << "Can't read factory from " << os_release << endl;
+	}
+}
+
+static void set_default_options(lmp_options &opt, string factory, string tags,
+				po::options_description &desc)
+{
+	bool prod = false;
+
+#if defined PRODUCTION
+	prod = true;
+#endif
+	desc.add_options()
+
+	("help", "print usage")
+	OPT_DEF_BOOL("use-ostree-server", opt.use_server, true, OSTREE_SRV_HELP)
+	OPT_DEF_BOOL("production,p", opt.production, prod, PRODUCTION_HELP)
+	OPT_DEF_BOOL("start-daemon", opt.start_daemon,true, DAEMON_HELP)
+	OPT_DEF_STR("sota-dir,d", opt.sota_dir, SOTA_DIR, SOTA_DIR_HELP)
+	OPT_STR("device-group,g", opt.device_group, DEVICE_GROUP_HELP)
+	OPT_DEF_STR("factory,f", opt.factory, factory, FACTORY_HELP)
+	OPT_STR("hsm-so-pin,S", opt.hsm_so_pin, HSM_SO_PIN_HELP)
+	OPT_DEF_BOOL("mlock-all,l", opt.mlock, true, MLOCK_HELP)
+	OPT_DEF_STR("hwid,i", opt.hwid, HARDWARE_ID, HWID_HELP)
+	OPT_DEF_STR("tags,t", opt.pacman_tags, tags, TAGS_HELP)
+	OPT_STR("api-token,T", opt.api_token, API_TOKEN_HELP)
+	OPT_STR("hsm-module,m", opt.hsm_module, HSM_HELP)
+	OPT_STR("hsm-pin,P", opt.hsm_pin, HSM_PIN_HELP)
+	OPT_STR("uuid,u", opt.uuid, UUID_HELP)
+	OPT_STR("name,n", opt.name, NAME_HELP)
+	OPT_DEF_STR("api-token-header,H",
+		    opt.api_token_header, "OSF-TOKEN",API_TOKEN_HDR_HELP)
+
+#if defined DOCKER_COMPOSE_APP
+	OPT_STR("apps,a", opt.apps, APPS_HELP)
+	/*
+	 * Enabled by default, list == compose_apps (or all Target apps)
+	 *
+	 * --restorable-apps "app-01[,app-02]"
+	 *       enable
+	 *       list == UNION(compose_apps, app-01[,app-02])
+	 *
+	 * --restorable-apps ""
+	 *       disable
+	 */
+	OPT_DEF_STR("restorable-apps,A", opt.restorable_apps," ",
+		    RESTORABLE_APP_HELP)
+#endif
+	;
+}
+
+static int parse_command_line(int argc, char **argv,
+			       po::options_description &desc)
+{
+	po::options_description all("lmp-device-register all options");
+	po::variables_map vm;
+
+	all.add(desc);
+	try {
+		po::store(
+			po::parse_command_line(
+				argc,
+				reinterpret_cast<const char *const *>(argv),
+				all),
+			vm);
+
+		if (vm.count("help")) {
+			cout << desc;
+			cout << "Git Commit " << GIT_COMMIT << endl;
+			return -1;
+		}
+		po::notify(vm);
+	} catch (const po::error &o) {
+		cout << "ERROR: " << o.what() << endl;
+		cout << endl << desc << endl;
+		return -1;
+	}
+
+	return 0;
+}
+
+static void get_uuid(lmp_options &opt)
+{
+	boost::uuids::uuid tmp;
+
+	/* Use PKCS#11 if the hsm_module was configured */
+	if (pkcs11_get_uuid(opt))
+		cerr << "WARN: can't get UUID from PKCS token" << endl;
+
+	if (opt.uuid.empty()) {
+		tmp = boost::uuids::random_generator()();
+		opt.uuid = boost::uuids::to_string(tmp);
+		cout << "UUID: " << opt.uuid <<" [Random]" << endl;
+	}
+}
+
+int options_parse(int argc, char **argv, lmp_options &opt)
+{
+	po::options_description desc("lmp-device-register options");
+	string factory;
+	string tags;
+
+	/* Read from environment or configuration file */
+	get_factory_tags_info(LMP_OS_STR, factory, tags);
+
+	set_default_options(opt, factory, tags, desc);
+
+	/* Command line takes precedence over any parameters */
+	if (parse_command_line(argc, argv, desc))
+		return -1;
+
+	if (opt.factory.empty()) {
+		cerr << "Missing factory definition" << endl;
+		return -1;
+	}
+
+	if (opt.pacman_tags.empty()) {
+		cerr << "Missing tag definition" << endl;
+		return -1;
+	}
+
+	if (!opt.hsm_module.empty())
+		if (opt.hsm_so_pin.empty() || opt.hsm_pin.empty()) {
+			cerr << "HSM incorrectly configured" << endl;
+			return -1;
+		}
+
+	if (opt.hsm_module.empty())
+		if (!opt.hsm_so_pin.empty() ||
+		    !opt.hsm_pin.empty()) {
+			cerr <<  "HSM incorrectly configured" << endl;
+			return -1;
+		}
+
+	/* Production env ENABLED takes precedence over config disabled */
+	opt.production = std::getenv(ENV_PRODUCTION) != nullptr ?
+			true : opt.production;
+
+	/* Set the UUID from OS-Release, HSM or RNG */
+	if (opt.uuid.empty())
+		get_uuid(opt);
+
+	/* Set the factory name from the UUID if not speficied */
+	if (opt.name.empty()) {
+		cout << "Setting factory name to UUID " << endl;
+		opt.name = opt.uuid;
+	}
+
+	if (opt.mlock) {
+		if (mlockall(MCL_CURRENT | MCL_FUTURE)) {
+			cout << "Error locking memory " << endl;
+			return -1;
+		}
+	}
+
+	cout << "PID memory " << (opt.mlock ? "locked" : "unlocked") <<  endl;
+
+	return 0;
+}

commit 8468ca3fcb4ea111e8062a16377514271a5d76dd

auth: Fix dump of http error replies when an 'error' field is used
This was originally fixed in:
ccd4499 (Print log message of each error, 2022-11-21)

But there was a regression in:
263d11e (main: refactor lmp-device-register, 2023-04-11)

Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

src/auth.cpp
@@ -16,8 +16,17 @@ static void dump_resp_error(string message, gint64 code, ptree resp)
 	if (resp.data().length())
 		cerr << resp.data() << endl;
 
-	for (const auto& it: resp)
-		cerr << it.first << ": " << it.second.data() << endl;
+	for (const auto& it: resp) {
+		const auto resp_elem{it.first};
+		if (resp_elem == "errors") {
+			cerr << resp_elem << ":" << endl;
+			for (auto it_err: it.second) {
+				cerr << '\t' << it_err.first << ": " << it_err.second.data() << endl;
+			}
+		} else {
+			cerr << it.first << ": " << it.second.data() << endl;
+		}
+	}
 }
 
 /* Returns the access_token on success, and empty string on errors */